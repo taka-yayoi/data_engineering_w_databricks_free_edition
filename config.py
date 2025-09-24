3c978d8614c873483389038add291227ea33bdb8
taka-yayoi <taka-yayoi>
2025-09-24 00:25:44 +0000

    changed to ipynb

--- config.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# カタログ名
-CATALOG_NAME = "workspace"
-
-# ボリューム名
-VOLUME_NAME = "de_volume"
-
-print(f"ハンズオンで使用するカタログは {CATALOG_NAME} です。")
-print(f"ハンズオンで使用するボリュームは {VOLUME_NAME} です。")
